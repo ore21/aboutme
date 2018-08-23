60dbd461077a015623fc56f6e63595ff20ea0f9a
Brandon <bgonzalez526@students.mchenry.edu>
2018-08-23 11:38:33 -0500

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println(" Hey my name is Brandon! ");
+        System.out.println(" I'm learning programming and i think is still one of the hardest things i've done.");
+        System.out.println(" I like android and designing apps have been my favorite thing in mind since i was 14. ");
+        System.out.println(" I've been a phone enthusiastic since the first android phones started coming out. ");
+        System.out.println(" I am hoping to get way better at programming then i'm currently at.");
+    }
+}
